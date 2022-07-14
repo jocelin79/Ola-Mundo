a145e8b63a54e5004938d6353ccdbb30c36b3799
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-14 13:04:45 -0300

    Update

---
+++ src/components/MySQL.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+	id INT NOT NULL PRIMARY KEY AUTOINCRMENT,
+	nome VARCHAR(20), NOTE NULL,
+	nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Nathally', '1990 05 22')
+
+SELECT * FROM 'pessoa'
+
