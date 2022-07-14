b6e4c319e6e606c75d589ada1f23b2360141ff33
Jocelin Nunes <91573405+jocelin79@users.noreply.github.com>
2022-07-14 15:59:37 -0300

    up

--- src/components/MySQL.sql
+++ src/components/MySQL.sql
@@ -1,10 +1,22 @@
 CREATE TABLE pessoas (
-	id INT NOT NULL PRIMARY KEY AUTOINCRMENT,
-	nome VARCHAR(20), NOTE NULL,
+	id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+	nome VARCHAR(20), NOT NULL,
 	nascimento DATE
-)
+);
 
-INSERT INTO pessoas (nome, nascimento) VALUES ('Nathally', '1990 05 22')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Nathally', '1990 05 22');
 
-SELECT * FROM 'pessoa'
+SELECT * FROM 'pessoa';
+SELECT nome FROM 'pessoa';
 
+UPDATE pessoa SET nome = 'Nathally Souza' WHERE id = 2;
+
+UPDATE pessoa SET nome = 'Marcela' WHERE id = 3;
+
+UPDATE pessoa SET nome = 'Flávio' WHERE id = 5;
+
+DELETE FROM pessoa WHERE id=5;
+
+SELECT * FROM pessoa ORDER BY nome;
+
+SELECT * FROM pessoa ORDER BY DESC
